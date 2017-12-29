d161e2946c4e1fa8f07a457472dac5b14c4a7c31
李杨 <liyang@liyangdeMacBook-Pro.local>
2017-12-29 11:13:19 +0800

    first

---
+++ front-src/entry/app.ts
@@ -0,0 +1,47 @@
+import '../style/style.scss';
+
+require('../_asset/20130605100532641.jpg')
+
+
+import { RemInit } from '../lib/Rem';
+import { routes } from './router';
+
+import { Page1 } from "./page-1/page-1";
+
+
+function initApp(){
+  RemInit();
+  routes.set('page1', new Page1());
+
+  
+
+  routes.router('page1', () => {
+    closePage();
+    routes.get('page1').show();
+  });
+  routes.router('page2', () => {
+    closePage();
+    routes.get('page2').show();
+  });
+  routes.router('page3', () => {
+    closePage();
+    routes.get('page3').show();
+  });
+  routes.router('page4', () => {
+    closePage();
+    routes.get('page4').show();
+  });
+  routes.router('page5', () => {
+    closePage();
+    routes.get('page5').show();
+  });
+}
+function closePage(){
+  routes.pages.forEach( page => {
+    page.page.hide();
+  })
+}
+
+initApp();
+
+routes.go('page1');
